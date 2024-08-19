65f92743eb9bc50ebf15ea6246b5e0d4a908873c
Commkom <Zzk6396515@outlook.com>
2024-08-19 17:43:40 +0800

    Update Windows-95-Key-Generator.py

--- Windows-95-Key-Generator.py
+++ Windows-95-Key-Generator.py
@@ -2,37 +2,36 @@
 import random
 
 def generate_serial():
-    # Éú³ÉÇ°3Î»£¬·¶Î§001µ½366
+    
+    # ç”Ÿæˆå‰ 3 ä½æ•°å­—ï¼ŒèŒƒå›´ä» 001 åˆ° 366
     # Generate the first 3 digits, ranging from 001 to 366
     first_three = str(random.randint(1, 366)).zfill(3)
 
-    # Éú³Éºó2Î»£¬·¶Î§95µ½03£¨Êµ¼ÊÉÏÓ¦¸ÃÊÇ95µ½99ºÍ00µ½03£©
+    # ç”Ÿæˆçš„æœ€å 2 ä½æ•°å­—èŒƒå›´ä» 95 åˆ° 03ï¼ˆå®é™…ä¸Šåº”è¯¥æ˜¯ 95 åˆ° 99 å’Œ 00 åˆ° 03ï¼‰
     # The last 2 digits generated range from 95 to 03 (actually it should be 95 to 99 and 00 to 03)
     year_suffix = str(random.randint(95, 99)).zfill(2)
 
-    # Éú³É3¸ö×ÖÄ¸£¬Ö»ÄÜÊÇOEM
-    # Generate 3 letters, only OEM
-    letters = 'OEM'
-
-    # Éú³É7¸öÊı×Ö£¬µÚÒ»Î»Îª0£¬ÆäÓàÏà¼ÓºóÄÜÕû³ı7
+    # ç”Ÿæˆ7ä¸ªæ•°å­—ï¼Œç¬¬ä¸€ä¸ªæ•°å­—æ˜¯0ï¼Œå…¶ä½™æ•°å­—ç›¸åŠ åå¯ä»¥è¢«7æ•´é™¤
     # Generate 7 numbers, the first digit is 0, and the rest can be divisible by 7 after adding them together
     first_digit = 0
-    remaining_digits = [random.randint(0, 9) for _ in range(5)]  # 5¸öËæ»úÊı×Ö # 5 random numbers
+    remaining_digits = [random.randint(0, 9) for _ in range(5)]  
     sum_of_digits = first_digit + sum(remaining_digits)
     last_digit = (7 - (sum_of_digits % 7)) % 7
     seven_digits = [first_digit] + remaining_digits + [last_digit]
     seven_digits_str = ''.join(map(str, seven_digits))
-    # Éú³É5Î»Ëæ»úÊı×Ö
+
+    # ç”Ÿæˆ 5 ä½éšæœºæ•°
     # Generate 5 digit random numbers
     random_numbers = ''.join([str(random.randint(0, 9)) for _ in range(5)])
 
-    # Æ´½Ó³ÉĞòÁĞºÅ¸ñÊ½£¬Ê¹ÓÃ¶Ì»®Ïß·Ö¸ô
+    # æ‹¼æ¥æˆåºåˆ—å·æ ¼å¼ï¼Œç”¨ç ´æŠ˜å·åˆ†éš”
     # Splice into serial number format, separated by dashes
-    serial_number = f"{first_three}{year_suffix}-{letters}-{seven_digits_str}-{random_numbers}"
+    serial_number = f"{first_three}{year_suffix}-OEM-{seven_digits_str}-{random_numbers}"
     
     return serial_number
 
+# ç”Ÿæˆåºåˆ—å·
 # Generate serial number
-# Éú³ÉĞòÁĞºÅ
 serial = generate_serial()
 print("Windows 95 key:", serial)
+input("Press Enter to exit...")
